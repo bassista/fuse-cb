f60f4e6371311ec3678220281c26ac7a83612058
openshiftio-launchpad <obsidian-leadership@redhat.com>
2018-01-26 20:34:34 +0100

    Initial commit

---
+++ name-service/src/main/java/org/jboss/fuse/boosters/cb/Application.java
@@ -0,0 +1,19 @@
+package org.jboss.fuse.boosters.cb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.undertow.UndertowBuilderCustomizer;
+import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    /**
+     * Main method to start the application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
